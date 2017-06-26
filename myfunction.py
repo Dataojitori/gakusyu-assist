7e5669da84340695ede1b142ab103545a7104c6f
ニワトリ <niwatorihayate@gmail.com>
2017-06-27 07:27:30 +0900

    第一次成功运行.可以保存和读取.并且创建了第一个问题数据

--- myfunction.py
+++ myfunction.py
@@ -1,33 +1,80 @@
 # -*- coding:utf-8 -*-
-import time, math
-import pickle
 
-def readfile(filename):
+import time
+import math
+import pickle
+import os
+import shutil
+
+
+def readfile(mypath, filename):
 	"读取文件内数据"
-	myfile=open(filename, "rb")
-	datas = pickle.load(f)
+	filename = mypath + "\\" + filename
+	myfile=open(filename, "r")
+	datas = pickle.load(myfile)
 	myfile.close()
 	return datas
 
-def writefile(filename, datas):
+def writefile(mypath, filename, datas):
 	"向文件内写入"
-	myfile = open(filename, "wb")
+	filename = mypath + "\\" + filename
+	myfile = open(filename, "w")
 	pickle.dump(datas, myfile)
 	myfile.close()
 
+def move_file(mypath, fileder, newfileder, newname):
+	"获取等待文件夹的文件并改名放入归档文件夹"
+	"返回更改后文件名"
+	#获取图片文件名
+	if os.listdir(mypath + "\\" + fileder):
+		oldname = os.listdir(mypath + "\\" + fileder)[0]
+		#获取图像后缀
+		imgtypeq = str.split(oldname,'.')[1]
+		#生成新文件名
+		newname = newname + '.' + imgtypeq
+		#切换工作目录
+		os.chdir(mypath + "\\" + fileder)
+		#重命名
+		os.rename(oldname, newname)
+		#移动文件归档
+		shutil.move(mypath + "\\" + fileder + "\\" + newname, mypath + "\\" + newfileder) 
+		return newname
+
+def get_big_number(mypath):
+	"获取问题文件夹里数值最大的文件名编号。如果没有文件则为0"
+	names = os.listdir(mypath + "\\questiondata")
+	maxnumber = 0
+	for name in names:
+		maxnumber = max(maxnumber, int(str.split(name,'.')[0]))
+	return maxnumber
+
+def make_question(mypath, datas, number = None):
+	"获取或生成问题对象"
+	if number:#如果是已存在的问题
+		question = datas.get(number)
+	else :
+		#计算配给编号
+		number = get_big_number(mypath) + 1
+		#归档文件
+		newname = move_file(mypath, "questionwaiting", "questiondata", str(number))
+		move_file(mypath, "answerwaiting", "answerdata", str(number))
+		#生成新类
+		question = mondai(newname)
+		datas[number] = question
+	return question
+	
 class mondai:
 	"""问题类，每个类存放一个问题的数据"""
 	"""公式为e^-(t/(3^n))。知识度n是3的指数。t时间单位是天"""
 	"""每个类的计算区别只取决于lasttime上次做题时间，以及understand理解度"""
-	def __init__(self, qus_img, ans_img):
-		self.qus_img = qus_img #♥
-		self.ans_img = ans_img #♥
+	def __init__(self, imgnum):
+		self.imgnum = imgnum #♥图片编号
 		self.tag = []
 		self.memo = []
-		self.history = [] #♥记录列表，包含子列表[时间，分数]
+		self.history = [] #♥记录列表，包含子列表(时间，分数)
 		self.lasttime = 0 #♥最后一次学习的时间，单位为1970以来的秒
 		self.understand = 0 #♥理解度,0~正无穷
-		self.remember = 0 #记忆度,0~1
+		self.remember = 0 #记忆度,0~1.不需要保存,即用即算
 
 	def write_memo(self, memo):
 		"记录文字笔记"
@@ -36,8 +83,9 @@ class mondai:
 	def whatis_remenber(self):
 		"返回现在的记忆程度百分比"
 		t = (time.time() - self.lasttime) / 86400 #自最后一次学习以来的天数
-		knowledge = 3^self.understand
-		self.remember = math.e^(-t / knowledge)
+		knowledge = 3**self.understand
+		self.remember = math.e**(-t / knowledge)
+		return self.remember
 
 	def return_time(self):
 		"返回多少时间后应该出题"
@@ -49,14 +97,15 @@ class mondai:
 		"不返回值。出题时间的类型是自1970年秒数"
 		pass
 
-	def score(self, point):
-		"解题后进行评分以及归档，根据评分更新self.understand理解度"
+	def write_history(self, point):
+		"向历史里记录成绩，根据评分更新self.understand理解度"
 		"point范围0~1，是正答率的百分比"
 		"point大于0.5，记忆度越低，理解度增加越高；point小于0.5，记忆度越高，理解度降低越大"
 		# if point > 0.5:
 		# 	zen = (1 - self.remember) * point 
 		# else :
 		# 	zen = -self.remember * (1 - point)
+		self.whatis_remenber() #更新现在记忆度
 		self.understand += point - self.remember	
 		self.wordtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
 		machinetime = time.time()
---
+++ play.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+from myfunction import *
+
+
+
+
+
+dataname = "his.txt"
+#定义路径
+mypath = os.getcwd()
+#读取数据
+datas = readfile(mypath, dataname)
+#计算新文件名编号
+maxnum = get_big_number(mypath) + 1
+#把问题归档
+#newname = move_file(mypath, "questionwaiting", "questiondata", str(maxnum))
+#move_file(mypath, "answerwaiting", "answerdata", str(maxnum))
+qus = make_question(mypath, datas) #一个问题类对象
+qus.write_history(0)
+writefile(mypath, dataname, datas)
+
+def datas_to_issue():
+	"从数据集里找出记忆度最低的问题"
+	def whoisbig(q1, q2):
+		if q1.whatis_remenber() < q2.whatis_remenber():
+			return q1
+		else :
+			return q2
+	question = reduce(whoisbig, datas.values())
+	return question
+		
