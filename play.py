bd8c12c75eaae0b107e2b4b984e29aed9e66c34d
ニワトリ <niwatorihayate@gmail.com>
2017-07-09 05:43:16 +0900

    定期备份

--- play.py
+++ play.py
@@ -9,12 +9,14 @@ mypath = os.getcwd()
 #读取数据
 datas = readfile(mypath, dataname)
 #计算新文件名编号
-def new(point):
+def new(point, tag = None):
 	"新建一个问题并写入得分"
 	maxnum = get_big_number(mypath) + 1
 	global qus
 	qus = make_question(mypath, datas) #一个问题类对象
 	qus.write_history(point)
+	if tag :
+		qus.tag.append(tag)
 	writefile(mypath, dataname, datas)
 
 def give():
@@ -24,11 +26,14 @@ def give():
 	qus = datas_to_issue(datas)			
 	imgname = qus.imgnum
 	shutil.move(mypath + "\\" + "questiondata" + "\\" + imgname, mypath + "\\" + "questionwaiting") 
+	shutil.move(mypath + "\\" + "answerdata" + "\\" + imgname, mypath + "\\" + "answerwaiting") 
 
 def back(point):
 	"记录分数并返还问题图片"
 	qus.write_history(point)
 	imgname = qus.imgnum
 	shutil.move(mypath + "\\" + "questionwaiting" + "\\" + imgname, mypath + "\\" + "questiondata") 
+	shutil.move(mypath + "\\" + "answerwaiting" + "\\" + imgname, mypath + "\\" + "answerdata") 
 	writefile(mypath, dataname, datas)
+
 
